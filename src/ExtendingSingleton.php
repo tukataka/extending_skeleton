5835e63c4dfa82865d8278db318b99763dfd53ba
Nikita Isakov <niki-isak@yandex.ru>
2016-06-21 13:59:43 +0300

    21/06/2016

--- src/ExtendingSingleton.php
+++ src/ExtendingSingleton.php
@@ -1,17 +1,17 @@
 <?php
 
-	abstract class ExtendingSingleton {
-        protected static $_instance = [];
+abstract class ExtendingSingleton {
+    protected static $_instance = [];
 
-        public static function getInstance() {
-            if (!isset(static::$_instance[static::class])) {
-                static::$_instance[static::class] = new static();
-            }
-
-            return static::$_instance[static::class];
+    public static function getInstance() {
+    	if (!isset(static::$_instance[static::class])) {
+            static::$_instance[static::class] = new static();
         }
 
-        protected function __clone() {}
-        protected function __construct() {}
-        protected function __wakeup() {}
+        return static::$_instance[static::class];
     }
+
+    protected function __clone() {}
+    protected function __construct() {}
+    protected function __wakeup() {}
+}
