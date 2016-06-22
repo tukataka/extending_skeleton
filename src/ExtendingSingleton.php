91b372f61f7ec0197f3981f085c671dfdab15775
nisakov <niki-isak@yandex.ru>
2016-06-22 18:53:48 +0300

    22/06/2016 18:53

--- src/ExtendingSingleton.php
+++ src/ExtendingSingleton.php
@@ -1,26 +1,25 @@
 <?php
 
-abstract class ExtendingSingleton {
+abstract class ExtendingSingleton
+{
     private static $_instance = [];
 
-    public static function getInstance() {
+    public static function getInstance()
+    {
         if (static::class == self::class) {
 	        return null;
 	    }
         
     	if (!isset(self::$_instance[static::class])) {
-            self::$_instance[static::class] = new static();
+            self::$_instance[static::class] = static::create();
         }
 
         return self::$_instance[static::class];
     }
 
-    public function __call($name, $arguments) {
-        return self::$_instance[static::class]->_container->$name($arguments);
-    }
-
-    public function __callStatic($name, $arguments) {
-        return self::$_instance[static::class]->_container::$name($arguments);
+    protected function create()
+    {
+        return null;
     }
 
     protected function __clone() {}
